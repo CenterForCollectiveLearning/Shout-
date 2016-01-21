e5e80a2423ed7acab1c9f554afb177d8624fc9fc
Ambika Krishnamachar <ambikak@mit.edu>
2016-01-20 21:06:23 -0300

    Changed trade request to free number entry

--- client/templates/profile_modal.js
+++ client/templates/profile_modal.js
@@ -1,3 +1,6 @@
+var MIN_TRADE_PARAM = 0;
+var MAX_TRADE_PARAM = 25;
+
 function has_current_trade_relationship(other_user_id) {
   var trades = Trades.find({"user_id": Meteor.userId(), "trades.other_user_id": other_user_id, "trades.this_trade_num":{$gt:0}}).fetch();
   var count = trades.length;  
@@ -7,7 +10,25 @@ function has_current_trade_relationship(other_user_id) {
   return true;
 }
 
+function checkOfferParam(param) {
+  if (isNaN(parseInt(param)) || param < MIN_TRADE_PARAM || param > MAX_TRADE_PARAM)
+  {
+    return false;
+  }
+  return true;
+}
+
 Template.profile_modal.helpers({
+  get_min_trade_param: function() {
+    return MIN_TRADE_PARAM;
+  },
+  get_max_trade_param: function() {
+    return MAX_TRADE_PARAM;
+  },
+
+  exists_param_error: function() {
+    return Session.get("paramError");
+  },
   other_user_timeline: function() {
     return Session.get("otherUserTimeline");
   },
@@ -46,12 +67,9 @@ Template.profile_modal.helpers({
   interests: function(user_id) {
     return Meteor.user() && Meteor.user().profile && Meteor.user().profile.interests;
   },
-    dateConverter: function(date) {
+  dateConverter: function(date) {
     return dateConverter(date);
   }
-
-
-
 });
 
 Template.profile_modal.events({
@@ -62,6 +80,17 @@ Template.profile_modal.events({
     var proposed_from = template.find('.num-you').value;
     var proposed_to = template.find('.num-them').value;
 
-    Meteor.call("updateCurrentTradeRequest", user_id_from, user_id_to, proposed_from, proposed_to);
+    if (checkOfferParam(proposed_from) && checkOfferParam(proposed_to)) {
+      Meteor.call("updateCurrentTradeRequest", user_id_from, user_id_to, proposed_from, proposed_to);
+      $('.modal').modal('hide');
+    }
+    else {
+      Session.set("paramError", true);
+    }
   },
+
+  'hidden.bs.modal .modal': function() {
+    Session.set("paramError", false);
+  }
+
 });
