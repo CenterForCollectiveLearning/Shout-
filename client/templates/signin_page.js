f0355ab698474073110d361a14041d6430bb9960
Ambika Krishnamachar <ambikak@mit.edu>
2016-04-14 10:52:59 -0400

    FIxed autoscroll problem on landing page (removed fullpage.js)

--- client/templates/signin_page.js
+++ client/templates/signin_page.js
@@ -1,10 +1,3 @@
-Template.signin_page.rendered = function() {
-	$('#fullpage').fullpage({
-		autoScrolling: false,
-		verticalCentered: false
-	});
-};
-
 Template.signin_page.events({
 	'click #btn-login': function() {
 		Meteor.loginWithTwitter(function(err, result) {
